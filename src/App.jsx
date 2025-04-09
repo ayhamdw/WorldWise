d3a4a7b4ddb1c882f6e26662c262f75b41a04b10
ayhamdw <s12029113@stu.najah.edu>
2025-04-09 18:53:48 +0200

    Add Map Component, Restyle and make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,12 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
-          <Route path="app" element={<AppLayout />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>List Of Cities</p>} />
+            <Route index path="cities" element={<p>List Of Cities</p>} />
+            <Route path="countries" element={<p>List Of Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,15 +2,14 @@
 // import Sidebar from "../components/Sidebar";
 // import User from "../components/User";
 
+import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
-
+import Map from "../components/Map";
 function AppLayout() {
   return (
     <div className={styles.app}>
-      {/* <Sidebar />
+      <Sidebar />
       <Map />
-      <User /> */}
-      <h1>App Layout Page</h1>
     </div>
   );
 }
