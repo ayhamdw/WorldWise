12eab9f066ed4656a3ce53706b1f248c3385bc5c
ayhamdw <s12029113@stu.najah.edu>
2025-04-09 19:33:39 +0200

    Add Cities with they info, make the style nice

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -6,8 +6,30 @@ import HomePage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+
+const URL = `http://localhost:9000/cities`;
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(URL);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("There was an error loading data... ", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
@@ -16,8 +38,12 @@ function App() {
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>List Of Cities</p>} />
-            <Route index path="cities" element={<p>List Of Cities</p>} />
+            <Route index element={<CityList />} />
+            <Route
+              index
+              path="cities"
+              element={<CityList cities={cities} loading={isLoading} />}
+            />
             <Route path="countries" element={<p>List Of Countries</p>} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import CityItem from "./CityItem";
+function CityList({ cities, loading }) {
+  if (loading) return <Spinner />;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
