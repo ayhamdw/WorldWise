db94b12faadc7df672e710739297bb7f40660779
ayhamdw <s12029113@stu.najah.edu>
2025-04-12 14:01:56 +0300

    Add map And start work with it

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Map.module.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useCities } from "../context/CitiesContext";
 function Map() {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+
+  const { cities } = useCities();
+
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      lat = {lat} , lng = {lng}
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
